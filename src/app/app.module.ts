7993ff9f05715c737bc1e82613a152077bffc5b4
afaki16 <alperen.faki@ithinka.com>
2023-08-10 11:57:01 +0300

    Create Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './denifition/layout/layout.component';
+import { AboutComponent } from './denifition/about/about.component';
+import { IntroComponent } from './denifition/intro/intro.component';
+import { SkillsComponent } from './denifition/skills/skills.component';
+import { ExperienceComponent } from './denifition/experience/experience.component';
+import { EducationComponent } from './denifition/education/education.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutComponent,
+    AboutComponent,
+    IntroComponent,
+    SkillsComponent,
+    ExperienceComponent,
+    EducationComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
